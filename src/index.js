d21b823228bc25068307850bb544d56ddaad7fba
Jairo Andres Rodriguez <48247042+AndresRodriguez27@users.noreply.github.com>
2021-08-31 01:31:56 +0800

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,46 @@
+const { app, BrowserWindow, Menu } = require('electron');
+const url = require('url');
+const path = require('path');
+const { title } = require('process');
+const { constants } = require('crypto');
+
+if(process.env.NODE_ENV !== 'production'){
+require('electron-reload')(__dirname, {
+electron: path.join(__dirname, '../node_modules', '.bin', 'electron')
+})
+}
+
+
+
+
+let mainWindow
+let Word
+let Excel
+let Powerpoint
+let PDF
+
+app.on('ready' , () => {
+mainWindow = new BrowserWindow({});
+mainWindow.loadURL(url.format({
+    pathname: path.join(__dirname, 'views/index.html'),
+    protocol: 'file',
+    slashes: true,
+    title: 'DoConver'
+}))
+
+
+const mainMenu = Menu.buildFromTemplate(templateMenu);
+Menu.setApplicationMenu(mainMenu);
+
+});
+
+
+const templateMenu = [
+{
+label: 'File'
+}
+
+];
+
+
+
